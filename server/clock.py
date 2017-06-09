a4dd772e700e74c4f3de62af28d553c0452d3b36
Luke Westby <hello@lukewestby.com>
2017-06-09 10:31:07 +0200

    some finishing touches

--- server/clock.py
+++ server/clock.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import sync_packages
+import os
 
 sched = BlockingScheduler()
+SYNC_INTERVAL = int(os.environ['PACKAGE_SYNC_INTERVAL_MINUTES'])
 
-@sched.scheduled_job('interval', minutes=15)
+@sched.scheduled_job('interval', minutes=SYNC_INTERVAL)
 def run_sync_packages():
     sync_packages.run()
 
