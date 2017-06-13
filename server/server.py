daf01997e4709ceebf906647072d320af55f3456
Luke Westby <hello@lukewestby.com>
2017-06-13 17:41:51 +0200

    python 3, mypy

--- server/server.py
+++ server/server.py
@@ -1,30 +1,38 @@
+from typing import Optional, Any, Optional, Iterator, TypeVar, List, NamedTuple, Dict, Tuple
 import json
 from flask import Flask, jsonify, request, render_template, url_for, redirect
 from werkzeug.routing import BaseConverter, ValidationError
 from datetime import datetime, timedelta
-from classes import Version, Constraint, PackageInfo, PackageName, ProjectId, ApiError, Package
 import boto3
 import botocore
 from itertools import *
 from operator import *
-import storage
-from urlparse import urlparse
-import assets
+from urllib.parse import urlparse
 import os
+from . import storage
+from . import assets
+from .classes import Version, Constraint, PackageInfo, PackageName, ProjectId, ApiError, Package
+
+T = TypeVar('T')
+
+def cat_optionals(data: Iterator[Optional[T]]) -> Iterator[T]:
+    for x in data:
+        if x is not None:
+            yield x
 
 app = Flask(__name__)
 
 class ProjectIdConverter(BaseConverter):
-    def to_python(self, value):
+    def to_python(self, value: str) -> Optional[ProjectId]:
         return ProjectId.from_string(value)
 
-    def to_url(self, value):
+    def to_url(self, value: ProjectId) -> str:
         return str(value)
 
 app.url_map.converters['project_id'] = ProjectIdConverter
 
 @app.errorhandler(ApiError)
-def handle_error(error):
+def handle_error(error: ApiError) -> Any:
     response = jsonify(None)
     response.status_code = error.status_code
     return response
@@ -41,51 +49,66 @@ all_versions = [
     Version(0, 15, 0)
 ]
 
-def organize_packages(packages):
-    data = {}
+class SearchablePackages(NamedTuple):
+    latest_by_elm_version: Dict[Version, Package]
+    versions: List[Version]
+
+class PackagesCache(NamedTuple):
+    last_updated: datetime
+    data: Dict[PackageName, SearchablePackages]
+
+def organize_packages(packages: List[PackageInfo]) -> Dict[PackageName, SearchablePackages]:
+    data: Dict[PackageName, SearchablePackages] = {}
     for package in packages:
         key = PackageName(package.username, package.package)
         if key not in data:
-            data[key] = {
-                'latest_by_elm_version': {},
-                'versions': []
-            }
+            data[key] = SearchablePackages({}, [])
 
-        data[key]['versions'].append(package.version)
+        data[key].versions.append(package.version)
 
         for version in all_versions:
-            if package.elm_constraint.is_satisfied(version):
-                if version not in data[key]['latest_by_elm_version']:
-                    data[key]['latest_by_elm_version'][version] = package.to_package()
-                if data[key]['latest_by_elm_version'][version].version < package.version:
-                    data[key]['latest_by_elm_version'][version] = package.to_package()
+            if package.elm_constraint is not None and package.elm_constraint.is_satisfied(version):
+                if version not in data[key].latest_by_elm_version:
+                    data[key].latest_by_elm_version[version] = package.to_package()
+                if data[key].latest_by_elm_version[version].version < package.version:
+                    data[key].latest_by_elm_version[version] = package.to_package()
     return data
 
-def get_searchable_packages():
+def get_searchable_packages() -> Dict[PackageName, SearchablePackages]:
     body = s3.Object('cdn.ellie-app.com', 'package-artifacts/searchable.json').get()['Body']
     data = body.read()
-    packages = [ PackageInfo.from_json(x) for x in json.loads(data) ]
+    packages = list(cat_optionals(PackageInfo.from_json(x) for x in json.loads(data)))
     body.close()
     return organize_packages(packages)
 
-def parse_int(string):
+def parse_int(string: str) -> Optional[int]:
     try:
         return int(string)
     except:
         return None
 
-cache_diff = timedelta(minutes=15)
+cache_diff: timedelta = timedelta(minutes=15)
 
-packages_cache = {
-    'last_updated': datetime.utcnow(),
-    'data': get_searchable_packages()
-}
+packages_cache: PackagesCache = PackagesCache(
+    datetime.utcnow(),
+    get_searchable_packages()
+)
 
-def package_entry_score(key, entries, elm_version, query):
+def refresh_packages_cache() -> None:
+    global packages_cache
+    now = datetime.utcnow()
+    if now - packages_cache.last_updated > cache_diff:
+        packages_cache = PackagesCache(
+            now,
+            get_searchable_packages()
+        )
+
+
+def package_entry_score(key: PackageName, entries: Dict[Version, Package], elm_version: Version, query: str) -> Tuple[Optional[Package], float]:
     if elm_version not in entries:
         return (None, 0)
 
-    score = 0
+    score = 0.0
     package = entries[elm_version]
 
     username_query = query
@@ -103,45 +126,38 @@ def package_entry_score(key, entries, elm_version, query):
 
     return (entries[elm_version], score)
 
-def do_search(elm_version, query):
-    score_gen = (package_entry_score(key, info['latest_by_elm_version'], elm_version, query) for key, info in packages_cache['data'].iteritems())
-    filtered = ifilter(lambda (p, s): s > 0, score_gen)
-    sorted_gen = sorted(list(filtered), key=lambda (p, s): -s)
-    return [p.to_json() for p, s in sorted_gen][0:5]
+def do_search(elm_version: Version, query: str) -> List[Any]:
+    score_gen = (package_entry_score(key, info.latest_by_elm_version, elm_version, query) for key, info in packages_cache.data.items())
+    filtered = filter(lambda t: t[1] > 0, score_gen)
+    sorted_gen = sorted(list(filtered), key = lambda t: -t[1])
+    return [p.to_json() for p, s in sorted_gen if p is not None][0:5]
 
 @app.route('/api/packages/<string:user>/<string:project>/versions')
-def tags(user, project):
-    now = datetime.utcnow()
-    if now - packages_cache['last_updated'] > cache_diff:
-        packages_cache['last_updated'] = now
-        packages_cache['data'] = get_searchable_packages()
-
+def tags(user: str, project: str) -> Any:
+    refresh_packages_cache()
     key = PackageName(user, project)
 
-    if key not in packages_cache['data']:
+    if key not in packages_cache.data:
         raise ApiError(404)
 
-    return jsonify([v.to_json() for v in packages_cache['data'][key]['versions']])
+    return jsonify([v.to_json() for v in packages_cache.data[key].versions])
 
 @app.route('/api/search')
-def search():
+def search() -> Any:
     query = request.args.get('query')
     if not isinstance(query, str):
-        pass
+        raise(ApiError(400))
     if len(query) <= 3:
-        pass
+        return jsonify([])
 
     elm_version = request.args.get('elmVersion')
     if not isinstance(elm_version, str):
-        pass
+        raise ApiError(400)
     parsed_elm_version = Version.from_string(elm_version)
     if parsed_elm_version is None:
-        pass
+        raise ApiError(400)
 
-    now = datetime.utcnow()
-    if now - packages_cache['last_updated'] > cache_diff:
-        packages_cache['last_updated'] = now
-        packages_cache['data'] = get_searchable_packages()
+    refresh_packages_cache()
 
     packages = do_search(parsed_elm_version, query)
 
@@ -149,7 +165,7 @@ def search():
 
 
 @app.route('/api/upload')
-def get_upload_urls():
+def get_upload_urls() -> Any:
     project_id_string = request.args.get('projectId')
 
     project_id = ProjectId.from_string(project_id_string) if (project_id_string is not None) else ProjectId.generate()
@@ -185,9 +201,10 @@ def get_upload_urls():
     return response
 
 @app.route('/api/revisions/default')
-def get_default_revision():
-    default_html = packages_cache['data'][PackageName('elm-lang', 'html')]['latest_by_elm_version'][Version(0, 18, 0)]
-    default_core = packages_cache['data'][PackageName('elm-lang', 'core')]['latest_by_elm_version'][Version(0, 18, 0)]
+def get_default_revision() -> Any:
+    refresh_packages_cache()
+    default_html = packages_cache.data[PackageName('elm-lang', 'html')].latest_by_elm_version[Version(0, 18, 0)]
+    default_core = packages_cache.data[PackageName('elm-lang', 'core')].latest_by_elm_version[Version(0, 18, 0)]
 
     return jsonify({
         'packages': [default_core.to_json(), default_html.to_json()],
@@ -223,15 +240,8 @@ main =
 '''
     })
 
-@app.route('/api/owned')
-def get_owned():
-    data = storage.get_owned_project_ids()
-    print data
-    out = jsonify([])
-    return out
-
 @app.route('/api/revisions/<project_id:project_id>/<int(min=0):revision_number>')
-def get_revision(project_id, revision_number):
+def get_revision(project_id: ProjectId, revision_number: int) -> Any:
     revision = storage.get_revision(project_id, revision_number)
     if revision is None:
         raise ApiError(404)
@@ -249,11 +259,11 @@ EDITOR_CONSTANTS = {
 
 @app.route('/')
 @app.route('/new')
-def new():
+def new() -> Any:
     return render_template('new.html', constants=EDITOR_CONSTANTS)
 
 @app.route('/<project_id:project_id>/<int(min=0):revision_number>')
-def existing(project_id, revision_number):
+def existing(project_id: ProjectId, revision_number: int) -> Any:
     if project_id.is_old:
         url = url_for('existing', project_id, revision_number)
         return redirect(url, code = 301)
@@ -263,8 +273,8 @@ def existing(project_id, revision_number):
         return redirect('new', code = 303)
 
     data = {
-        'title': revision['title'],
-        'description': revision['description'],
+        'title': revision.title,
+        'description': revision.description,
         'url': EDITOR_CONSTANTS['SERVER_HOSTNAME'] + '/' + str(project_id) + '/' + str(revision_number)
     }
 
@@ -282,22 +292,22 @@ EMBED_CONSTANTS = {
 }
 
 @app.route('/embed/<project_id:project_id>/<int(min=0):revision_number>')
-def embed(project_id, revision_number):
+def embed(project_id: ProjectId, revision_number: int) -> Any:
     if project_id.is_old:
         url = url_for('embed', project_id, revision_number)
-        return redicer(url, code = 301)
+        return redirect(url, code = 301)
 
     data = {}
     revision = storage.get_revision(project_id, revision_number)
     if revision is not None:
-        data['title'] = revision['title']
-        data['description'] = revision['description']
-        data['url'] = EBMED_CONSTANTS['SERVER_HOSTNAME'] + '/embed/' + str(project_id) + '/' + str(revision_number)
+        data['title'] = revision.title
+        data['description'] = revision.description
+        data['url'] = EMBED_CONSTANTS['SERVER_HOSTNAME'] + '/embed/' + str(project_id) + '/' + str(revision_number)
 
     return render_template('embed.html', constants=EMBED_CONSTANTS, data=data)
 
 @app.route('/oembed')
-def oembed():
+def oembed() -> Any:
     url = request.args.get('url')
     width_param = parse_int(request.args.get('width'))
     height_param = parse_int(request.args.get('height'))
@@ -332,8 +342,8 @@ def oembed():
         'height': 'height',
         'type': 'rich',
         'version': '1.0',
-        'title': revision['title'],
+        'title': revision.title,
         'provider_name': 'ellie-app.com',
         'provider_url': 'https://ellie-app.com',
-        'html': '<iframe src="'+ EDITOR_CONSTANTS['SERVER_HOSTNAME'] + '/embed/' + str(project_id) + '/' + str(revision_number) + '" width=' + str(width) + ' height=' + str(height) + ' frameBorder="0" allowtransparency="true"></iframe>'
+        'html': '<iframe src="' + EDITOR_CONSTANTS['SERVER_HOSTNAME'] + '/embed/' + str(project_id) + '/' + str(revision_number) + '" width=' + str(width) + ' height=' + str(height) + ' frameBorder="0" allowtransparency="true"></iframe>'
     })
