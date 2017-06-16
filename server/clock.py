177c7758e677faab1fc8614d1c46cba6528a4741
Luke Westby <hello@lukewestby.com>
2017-06-16 16:21:14 -0700

    use thread instead of subproc

--- server/clock.py
+++ server/clock.py
@@ -1,17 +1,21 @@
 import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from . import sync_packages
+import threading
 import os
-import subprocess
 
 logging.basicConfig()
 
-sched = BlockingScheduler()
-SYNC_INTERVAL = int(os.environ['PACKAGE_SYNC_INTERVAL_MINUTES'])
+def my_threaded_func() -> None:
+    SYNC_INTERVAL = int(os.environ['PACKAGE_SYNC_INTERVAL_MINUTES'])
+    sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=SYNC_INTERVAL)
-def run_sync_packages() -> None:
-    subprocess.check_call(["python", "./server/sync_packages.py"])
+    @sched.scheduled_job('interval', minutes=SYNC_INTERVAL)
+    def run_sync_packages() -> None:
+        sync_packages.run()
 
-def start_clock() -> None:
     sched.start()
+
+def start() -> None:
+    thread = threading.Thread(target=my_threaded_func)
+    thread.start()
