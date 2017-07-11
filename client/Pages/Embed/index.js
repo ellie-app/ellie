3b7c838e04b8322517483b65ba5ea193439b5b4f
Luke Westby <hello@lukewestby.com>
2017-07-10 19:06:17 -0500

    fix build

--- client/Pages/Embed/index.js
+++ client/Pages/Embed/index.js
@@ -1,9 +1,9 @@
 import 'es6-promise/auto'
-import '../../../elm/Apps/Embed/Stylesheets.elm'
-import initCodeMirror from '../../Shared/CodeMirror'
+import './Stylesheets.elm'
+import initCodeMirror from '../../Views/Editors/CodeMirror'
 
 initCodeMirror()
   .then(() => {
-    const Elm = require('../../../elm/Apps/Embed/Main.elm')
+    const Elm = require('./Main.elm')
     Elm.Pages.Embed.Main.fullscreen()
   })
