7794b6ff4ee709777ae274bd396d78abf740f1d2
Luke Westby <hello@lukewestby.com>
2017-07-13 10:15:59 -0500

    clean up clock code and add better logs to sync

--- server/clock.py
+++ server/clock.py
@@ -1,15 +1,11 @@
-import logging
 import os
-import threading
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from . import sync_packages
 
-logging.basicConfig()
-
 SYNC_INTERVAL = int(os.environ['PACKAGE_SYNC_INTERVAL_MINUTES'])
-sched = BackgroundScheduler()
+sched = BackgroundScheduler(daemon=False)
 
 
 @sched.scheduled_job('interval', minutes=SYNC_INTERVAL)
