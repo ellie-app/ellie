3f9c166ed000396e0c516b657180616804a2b8c0
Luke Westby <hello@lukewestby.com>
2017-12-21 10:43:44 -0800

    env variables in purescript build

---
+++ make/0.18.0/src/Constants.js
@@ -0,0 +1,2 @@
+exports.environment = process.env.NODE_ENV
+exports.cdnBase = process.env.CDN_BASE
--- make/0.18.0/src/Constants.purs
+++ make/0.18.0/src/Constants.purs
@@ -1,4 +1,4 @@
 module Constants where
 
-cdnBase :: String
-cdnBase = "https://s3.us-east-2.amazonaws.com/development-cdn.ellie-app.com"
+foreign import environment :: String
+foreign import cdnBase :: String
