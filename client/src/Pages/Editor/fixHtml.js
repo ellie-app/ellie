4f29a7d5ba706c13c9929917ce6aae2b9a464718
Derek Shoemaker <shoemakerdr@gmail.com>
2018-01-10 19:29:43 -0600

    Replaced prepend with insertBefore. Fixes #32

--- client/src/Pages/Editor/fixHtml.js
+++ client/src/Pages/Editor/fixHtml.js
@@ -91,7 +91,8 @@ export default ({ htmlCode, embedApi, sourceScript }) => {
 
       const div = doc.createElement('div')
       div.innerHTML = sourceScript
-      doc.body.prepend(div.children[0])
+      firstChild = doc.body.children[0]
+      doc.body.insertBefore(div.children[0], firstChild)
       var blob = new Blob([doc.documentElement.outerHTML], { type: 'text/html' })
       resolve(URL.createObjectURL(blob))
     } catch (error) {
