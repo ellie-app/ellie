14b3fa3f7597c6db571369684273b2841bad9251
Luke Westby <luke@noredink.com>
2018-04-23 11:22:55 -0700

    skip install in build script

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -2,15 +2,14 @@ echo "RUNNING STATIC BUILD"
 echo "1) Entering Phoenix directory: $phoenix_dir"
 cd $phoenix_dir
 
-echo "2) Installing NPM dependencies"
-npm --prefix ./assets install
-
-echo "3) Generating GraphQL client"
+echo "2) Generating GraphQL client"
 mix ellie.graphql
 
-echo "4) Building client applications"
-npm --prefix ./assets run build
+echo "3) Building client applications"
+pushd assets
+yarn run build
+popd
 mix "${phoenix_ex}.digest"
 
-echo "5) Downloading runtime executables"
-mix ellie.binstall
+echo "[SKIP] 4) Downloading runtime executables"
+# mix ellie.binstall
