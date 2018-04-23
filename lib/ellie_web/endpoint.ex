74494023d7ea7b70732c8209b3ef126c7f7ab640
Luke Westby <luke@noredink.com>
2018-04-23 13:10:13 -0700

    change static path

--- lib/ellie_web/endpoint.ex
+++ lib/ellie_web/endpoint.ex
@@ -9,8 +9,8 @@ defmodule EllieWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :ellie, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/static", from: :ellie, gzip: true,
+    only: ~w(css fonts images js svg favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
