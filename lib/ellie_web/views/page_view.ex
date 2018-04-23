47114b9770586defa2c005e8df22b5e06c980065
Luke Westby <luke@noredink.com>
2018-04-23 11:35:30 -0700

    use static assets in prod

--- lib/ellie_web/views/page_view.ex
+++ lib/ellie_web/views/page_view.ex
@@ -4,11 +4,17 @@ defmodule EllieWeb.PageView do
   def production?, do: Application.get_env(:ellie, :env) == :prod
   def development?, do: Application.get_env(:ellie, :env) == :dev
 
-  def asset(:editor_js), do: webpack_path("editor.js")
-  def asset(:editor_css), do: webpack_path("editor.css")
-  def asset(:embed_js), do: webpack_path("embed.js")
-  def asset(:embed_css), do: webpack_path("embed.css")
-  def asset(_), do: ""
+  def asset(conn, :editor_js), do: handle_relative(conn, "editor.js")
+  def asset(conn, :editor_css), do: handle_relative(conn, "editor.css")
+  def asset(conn, :embed_js), do: handle_relative(conn, "embed.js")
+  def asset(conn, :embed_css), do: handle_relative(conn, "embed.css")
+  def asset(_, _), do: ""
 
-  defp webpack_path(relative), do: "http://localhost:8080/" <> relative
+  defp handle_relative(conn, relative) do
+    if production?() do
+      static_path(conn, relative)
+    else
+      "http://localhost:8080/" <> relative
+    end
+  end
 end
