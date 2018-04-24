cd12eef76d1ba7389455e82318dcb67cabde8118
Luke Westby <luke@noredink.com>
2018-04-24 13:02:06 -0700

    install gigalixir in before_install

--- scripts/ci/deploy_pr.sh
+++ scripts/ci/deploy_pr.sh
@@ -2,28 +2,25 @@
 
 if [ $TRAVIS_PULL_REQUEST ]
 then
-    total_steps=6
+    total_steps=5
     echo "Deploying app for pull request"
-    echo "(1/$total_steps) Installing Gigalixir CLI"
-    pip install --user gigalixir
-
-    echo "(2/$total_steps) Logging into gigalixir"
+    echo "(1/$total_steps) Logging into gigalixir"
     gigalixir login -e $GIGALIXIR_EMAIL -y -p $GIGALIXIR_PASSWORD
 
     app_name="$GIGALIXIR_APP_NAME-dev-$TRAVIS_PULL_REQUEST"
-    echo "(3/$total_steps) Creating gigalixir app $app_name"
+    echo "(2/$total_steps) Creating gigalixir app $app_name"
     gigalixir create --name $app_name || gigalixir set_git_remote $app_name
 
-    echo "(4/$total_steps) Setting environment variables" 
+    echo "(3/$total_steps) Setting environment variables" 
     gigalixir set_config $app_name DATABASE_URL $DEV_DATABASE_URL
     gigalixir set_config $app_name ELM_19_BINARY_URL $DEV_ELM_19_BINARY_URL
     gigalixir set_config $app_name PACKAGE_SITE $DEV_PACKAGE_SITE
     gigalixir set_config $app_name SERVER_ORIGIN "https://$app_name.gigalixirapp.com"
     gigalixir set_config $app_name SOCKET_ORIGIN "wss://$app_name.gigalixirapp.com"
 
-    echo "(5/$total_steps) Building on Gigalixir"
+    echo "(4/$total_steps) Building on Gigalixir"
     git push -f gigalixir HEAD:refs/heads/master
     
-    echo "(6/$total_steps) Running database migrations"
+    echo "(5/$total_steps) Running database migrations"
     gigalixir migrate $app_name
 fi
