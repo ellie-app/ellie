cddc45cbc4d4cf9feb3a296e34d90891949fd443
Brandon Ashley <tb@tba.sh>
2018-09-18 18:09:37 -0400

    fix(config/prod): correct prod application env

--- config/prod.exs
+++ config/prod.exs
@@ -3,13 +3,9 @@ use Mix.Config
 config :logger, backends: []
 
 config :ellie, EllieWeb.Endpoint,
-  load_from_system_env: true,
   cache_static_manifest: "priv/static/manifest.json",
-  http: [port: {:system, "PORT"}],
-  url: [host: "localhost", port: {:system, "PORT"}],
   server: true,
-  root: ".",
-  version: Application.spec(:phoenix_distillery, :vsn)
+  version: Application.spec(:ellie, :vsn)
 
 config :ellie, Ellie.Repo,
   adapter: Ecto.Adapters.Postgres,
