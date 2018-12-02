874a07c2fe9f141e1fd74bdf2907e0b8a836b09a
Brandon Ashley <tb@tba.sh>
2018-12-02 14:03:42 -0500

    chore(*): force ssl in prod

--- config/prod.exs
+++ config/prod.exs
@@ -3,6 +3,7 @@ use Mix.Config
 config :logger, backends: []
 
 config :ellie, EllieWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
   version: Application.spec(:ellie, :vsn)
