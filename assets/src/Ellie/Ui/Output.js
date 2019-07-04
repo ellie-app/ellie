cbe278bf1d9343533cb604002b5e13bc8c2e0bbb
Brandon Ashley <tb@tba.sh>
2019-07-03 20:24:34 -0400

    fix(Ellie.Ui.Output): change src to about:blank for inner iframe

--- assets/src/Ellie/Ui/Output.js
+++ assets/src/Ellie/Ui/Output.js
@@ -1,24 +1,25 @@
-import CustomElements from '../../Platform/CustomElements'
+import CustomElements from "../../Platform/CustomElements";
 
 export default {
   start(app) {
-    app.ports.ellieUiOutputOut.subscribe((data) => {
+    app.ports.ellieUiOutputOut.subscribe(data => {
       switch (data.tag) {
-        case 'Reload':
+        case "Reload":
           requestAnimationFrame(() => {
-            document.querySelectorAll('ellie-ui-output[data-connected]').forEach((el) => {
-              el.reload()
-            })
-          })
-          return
+            document
+              .querySelectorAll("ellie-ui-output[data-connected]")
+              .forEach(el => {
+                el.reload();
+              });
+          });
+          return;
       }
-    })
+    });
 
-    const parser = new DOMParser()
-    const iframe = document.createElement('iframe')
+    const parser = new DOMParser();
+    const iframe = document.createElement("iframe");
 
-    const embeddedApi =
-    `(function () {
+    const embeddedApi = `(function () {
       var parent = window.parent
       delete window.parent
 
@@ -31,7 +32,9 @@ export default {
           var split = firstArg.split(': ')
           var label = split[0]
           var body = split.slice(1).join(': ')
-          parent.postMessage({ tag: 'LogReceived', contents: { label: label, body: body } }, '${window.location.origin}')
+          parent.postMessage({ tag: 'LogReceived', contents: { label: label, body: body } }, '${
+            window.location.origin
+          }')
         }
         oldLog.apply(this, arguments)
       }
@@ -49,7 +52,7 @@ export default {
         iframe.style.left = 0
         iframe.style.border = 0
         iframe.style.backgroundColor = '#fff'
-        iframe.src = 'javascript:void(0);'
+        iframe.src = 'about:blank'
         document.body.appendChild(iframe)
         var onClose = function () {}
         debuggerWindowProxy = Object.defineProperties({}, {
@@ -109,16 +112,16 @@ export default {
           }, '${window.location.origin}')
         }, 100)
       })
-    }())`
+    }())`;
 
     const wrapOnReady = (random, script) => {
-      return '__ellie_onReady_' + random + '(function () {' + script + '})'
-    }
+      return "__ellie_onReady_" + random + "(function () {" + script + "})";
+    };
 
-    const embedApi = (doc) => {
-      const script = doc.createElement('script')
-      script.textContent = embeddedApi
-      const style = doc.createElement('style')
+    const embedApi = doc => {
+      const script = doc.createElement("script");
+      script.textContent = embeddedApi;
+      const style = doc.createElement("style");
       style.textContent = `
         div[style*="z-index: 2147483646"][style*="background-color: rgba(200, 200, 200, 0.7)"] {
           position: fixed;
@@ -130,15 +133,14 @@ export default {
         }
         .elm-mini-controls {
           display: none !important;
-        }`
-      doc.head.appendChild(script)
-      doc.head.appendChild(style)
-    }
+        }`;
+      doc.head.appendChild(script);
+      doc.head.appendChild(style);
+    };
 
     const onReady = (doc, marker) => {
-      const script = doc.createElement('script')
-      script.textContent =
-        `function __ellie_onReady_${marker}(cb) {
+      const script = doc.createElement("script");
+      script.textContent = `function __ellie_onReady_${marker}(cb) {
           function completed() {
             document.removeEventListener("DOMContentLoaded", completed)
             window.removeEventListener("load", completed)
@@ -150,35 +152,35 @@ export default {
             document.addEventListener("DOMContentLoaded", completed)
             window.addEventListener("load", completed)
           }
-        }`
-      doc.head.appendChild(script)
-    }
+        }`;
+      doc.head.appendChild(script);
+    };
 
     const fixHtml = (htmlCode, scriptSrc) => {
-      const doc = parser.parseFromString(htmlCode, 'text/html')
+      const doc = parser.parseFromString(htmlCode, "text/html");
 
-      const random = Math.floor(9007199254740991 * Math.random())
+      const random = Math.floor(9007199254740991 * Math.random());
 
-      doc.querySelectorAll('script').forEach(el => {
-        if (el.getAttribute('src') !== null && !el.textContent.trim()) return
-        el.textContent = wrapOnReady(random, el.textContent)
-      })
+      doc.querySelectorAll("script").forEach(el => {
+        if (el.getAttribute("src") !== null && !el.textContent.trim()) return;
+        el.textContent = wrapOnReady(random, el.textContent);
+      });
 
-      embedApi(doc)
-      onReady(doc, random)
+      embedApi(doc);
+      onReady(doc, random);
 
-      const elmScript = doc.createElement('script')
-      elmScript.src = scriptSrc
-      doc.head.appendChild(elmScript)
+      const elmScript = doc.createElement("script");
+      elmScript.src = scriptSrc;
+      doc.head.appendChild(elmScript);
 
-      const charset = doc.createElement('meta')
-      charset.setAttribute('charset', 'utf-8')
-      doc.head.insertBefore(charset, doc.head.children[0])
+      const charset = doc.createElement("meta");
+      charset.setAttribute("charset", "utf-8");
+      doc.head.insertBefore(charset, doc.head.children[0]);
 
-      const finalHtml = '<!DOCTYPE html>' + doc.documentElement.outerHTML
-      var blob = new Blob([finalHtml], { type: 'text/html' })
-      return URL.createObjectURL(blob)
-    }
+      const finalHtml = "<!DOCTYPE html>" + doc.documentElement.outerHTML;
+      var blob = new Blob([finalHtml], { type: "text/html" });
+      return URL.createObjectURL(blob);
+    };
 
     // const convertStringToArrayBufferView = (string) => {
     //   return new Promise((resolve) => {
@@ -204,105 +206,131 @@ export default {
     //   return hex
     // }
 
-    CustomElements.define('ellie-ui-output', (HTMLElement) => class extends HTMLElement {
-      constructor() {
-        super()
-        this._html = null
-        this._elmSource = null
-        this._debug = false
-        this._url = null
-        this._attached = false
-        this._onMessage = this._onMessage.bind(this)
-        this._idleCallback = null
-      }
+    CustomElements.define(
+      "ellie-ui-output",
+      HTMLElement =>
+        class extends HTMLElement {
+          constructor() {
+            super();
+            this._html = null;
+            this._elmSource = null;
+            this._debug = false;
+            this._url = null;
+            this._attached = false;
+            this._onMessage = this._onMessage.bind(this);
+            this._idleCallback = null;
+          }
 
-      get html() {
-        return this._html
-      }
+          get html() {
+            return this._html;
+          }
 
-      set html(value) {
-        if (this._html === value) return
-        this._html = value
-        if (!this._attached) return
-        this._update()
-      }
+          set html(value) {
+            if (this._html === value) return;
+            this._html = value;
+            if (!this._attached) return;
+            this._update();
+          }
 
-      get elmSource() {
-        return this._elmSource
-      }
+          get elmSource() {
+            return this._elmSource;
+          }
 
-      set elmSource(value) {
-        if (this._elmSource === value) return
-        this._elmSource = value
-        if (!this._attached) return
-        this._update()
-      }
+          set elmSource(value) {
+            if (this._elmSource === value) return;
+            this._elmSource = value;
+            if (!this._attached) return;
+            this._update();
+          }
 
-      get debug() {
-        return this._debug
-      }
+          get debug() {
+            return this._debug;
+          }
 
-      set debug(value) {
-        this._debug = value
-        if (!this._attached) return
-        if (value) {
-          iframe.contentWindow.postMessage({ tag: 'SwitchToDebugger' }, window.location.origin)
-        } else {
-          iframe.contentWindow.postMessage({ tag: 'SwitchToProgram' }, window.location.origin)
+          set debug(value) {
+            this._debug = value;
+            if (!this._attached) return;
+            if (value) {
+              iframe.contentWindow.postMessage(
+                { tag: "SwitchToDebugger" },
+                window.location.origin
+              );
+            } else {
+              iframe.contentWindow.postMessage(
+                { tag: "SwitchToProgram" },
+                window.location.origin
+              );
+            }
+          }
+
+          connectedCallback() {
+            this.appendChild(iframe);
+            this._attached = true;
+            window.addEventListener("message", this._onMessage);
+            iframe.addEventListener("load", this._onLoad);
+            this._update();
+            this.setAttribute("data-connected", "");
+          }
+
+          disconnectedCallback() {
+            window.removeEventListener("message", this._onMessage);
+            this.removeChild(iframe);
+            cancelIdleCallback(this._idleCallback);
+            this.removeAttribute("data-connected");
+          }
+
+          reload() {
+            iframe.src = iframe.src;
+          }
+
+          _onMessage(e) {
+            switch (e.data.tag) {
+              case "LogReceived":
+                this.dispatchEvent(
+                  new CustomEvent("log", { detail: e.data.contents })
+                );
+                return;
+              case "Ready":
+                this._canDebug = e.data.contents.canDebug;
+                this.dispatchEvent(
+                  new CustomEvent("canDebug", {
+                    detail: e.data.contents.canDebug
+                  })
+                );
+                this._onReady();
+                return;
+              case "RuntimeException":
+                this.dispatchEvent(
+                  new CustomEvent("runtimeException", {
+                    detail: e.data.contents.message
+                  })
+                );
+            }
+          }
+
+          _onReady() {
+            if (this._debug && this._canDebug) {
+              iframe.contentWindow.postMessage(
+                { tag: "SwitchToDebugger" },
+                window.location.origin
+              );
+            } else {
+              iframe.contentWindow.postMessage(
+                { tag: "SwitchToProgram" },
+                window.location.origin
+              );
+            }
+          }
+
+          _update() {
+            cancelIdleCallback(this._idleCallback);
+            this._idleCallback = requestIdleCallback(() => {
+              URL.revokeObjectURL(this._url);
+              this._url = fixHtml(this._html, this._elmSource);
+              iframe.src = this._url;
+            });
+          }
         }
-      }
-
-      connectedCallback() {
-        this.appendChild(iframe)
-        this._attached = true
-        window.addEventListener('message', this._onMessage)
-        iframe.addEventListener('load', this._onLoad)
-        this._update()
-        this.setAttribute('data-connected', '')
-      }
-
-      disconnectedCallback() {
-        window.removeEventListener('message', this._onMessage)
-        this.removeChild(iframe)
-        cancelIdleCallback(this._idleCallback)
-        this.removeAttribute('data-connected')
-      }
-
-      reload() {
-        iframe.src = iframe.src
-      }
-
-      _onMessage(e) {
-        switch (e.data.tag) {
-          case 'LogReceived':
-            this.dispatchEvent(new CustomEvent('log', { detail: e.data.contents }))
-            return
-          case 'Ready':
-            this._canDebug = e.data.contents.canDebug
-            this.dispatchEvent(new CustomEvent('canDebug', { detail: e.data.contents.canDebug }))
-            this._onReady()
-            return
-          case 'RuntimeException':
-            this.dispatchEvent(new CustomEvent('runtimeException', { detail: e.data.contents.message }))
-        }
-      }
-
-      _onReady() {
-        if (this._debug && this._canDebug) {
-          iframe.contentWindow.postMessage({ tag: 'SwitchToDebugger' }, window.location.origin)
-        } else {
-          iframe.contentWindow.postMessage({ tag: 'SwitchToProgram' }, window.location.origin)
-        }
-      }
-
-      _update() {
-        cancelIdleCallback(this._idleCallback)
-        this._idleCallback = requestIdleCallback(() => {
-          URL.revokeObjectURL(this._url)
-          this._url = fixHtml(this._html, this._elmSource)
-          iframe.src = this._url
-        })
-      }
-    })
+    );
   }
-}
+};
