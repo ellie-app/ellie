58983322eda4cdd5bd9392a825fd9d3223a42c80
Geoff Langenderfer <lgeoff@amazon.com>
2022-05-01 12:33:45 -0700

    upgrade to elixir 1.13.4

--- lib/ellie_web/endpoint.ex
+++ lib/ellie_web/endpoint.ex
@@ -25,7 +25,7 @@ defmodule EllieWeb.Endpoint do
   plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Poison
+    json_decoder: Jason
   )
 
   plug(Plug.MethodOverride)
